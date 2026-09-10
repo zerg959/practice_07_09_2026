f705d653a9741f02f907a2f9cb00825c466e6329
zerg959 <zerg959@yandex.ru>
2026-09-10 11:00:49 +0300

    init

---
+++ script.sql
@@ -0,0 +1,25 @@
+create table if not exists partners (
+	id serial primary key,
+	inn varchar(12) not null unique,
+	email varchar(50) not null
+);
+create table if not exists products (
+	id serial primary key,
+	article varchar(50) not null unique,
+	product_name varchar(100) not null,
+	description varchar(300)
+);
+create table if not exists orders (
+	id serial primary key,
+	order_code varchar(50) not null,
+	partner_id integer references partners(id)
+);
+create table if not exists order_items (
+	id serial primary key,
+	order_id integer not null references orders(id) on delete cascade,
+	product_id integer references products(id) on delete restrict,
+	quantity integer not null check (quantity > 0),
+	price decimal(10,2) not null check (price > 0),
+	total decimal(10,2) generated always as (quantity * price) stored,
+	created_at timestamptz default now() not null
+);
